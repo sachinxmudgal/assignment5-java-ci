0a43dde79154614159a20cf2a1da2e649918ebd3
Sachin <sachin1420sm@gmail.com>
2026-05-29 11:49:04 +0530

    Initial Java project for Assignment 5

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Assignment 5 Project");
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
